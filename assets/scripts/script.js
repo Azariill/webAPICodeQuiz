5b311958648636e19734142c6fdc45036094452b
Azarill <heierms@gmail.com>
2022-04-07 23:09:42 -0500

    finished front page/started js.script

---
+++ assets/scripts/script.js
@@ -0,0 +1,19 @@
+var container = document.querySelector(".main-flex");
+var timer = 80;
+var divOne = document.querySelector(".div1");
+var divTwo = document.querySelector(".div2");
+
+
+container.addEventListener("click", function(event) {
+    var element = event.target;
+    var startButton = document.querySelector(".start-button");
+
+    if(element === startButton){
+        divOne.innerHTML ="";
+        divTwo.innerHTML ="";
+
+    }
+
+    console.log(element);
+});
+
